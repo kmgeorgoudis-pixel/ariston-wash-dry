4b88a8b765190c9cf3f50845969321228a023032
kmgeorgoudis-pixel <kmgeorgoudis@gmail.com>
2026-01-18 21:13:02 +0200

    fix en updates

--- app.py
+++ app.py
@@ -1872,7 +1872,7 @@ Your data is used solely for the operation of the service and is not shared with
 def goodbye_en():
     return render_template("goodbye-en.html")
 
-@app.route("/en/updates-menu")
+@app.route("/en/updates-menu-en")
 @login_required
 def updates_menu_en():
     return render_template("updates-menu-en.html")
