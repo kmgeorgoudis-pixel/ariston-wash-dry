3c0fdabe3fe0f54964fb61a9229e75b8a76193c9
kmgeorgoudis-pixel <kmgeorgoudis@gmail.com>
2026-02-16 17:19:27 +0200

    big update 83 sos

--- app.py
+++ app.py
@@ -2289,5 +2289,5 @@ def site_review_en():
 # ============================
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
